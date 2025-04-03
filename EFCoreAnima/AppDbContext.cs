da6ea04bfcadec15bec71b9f55b14d686f94d484
FreeOnur <144993689+FreeOnur@users.noreply.github.com>
2025-04-03 23:20:04 +0200

    Created First Entity Framework Database

---
+++ EFCoreAnima/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreAnima
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Animal> Animals { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EFCoreAnimaDB;Trusted_Connection=True;");
+        }
+    }
+}

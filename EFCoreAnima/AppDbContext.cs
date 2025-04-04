2f02e32b1e579ff96740dde5e62411d188c04620
FreeOnur <144993689+FreeOnur@users.noreply.github.com>
2025-04-04 14:17:29 +0200

    Finished DBExtended

--- EFCoreAnima/AppDbContext.cs
+++ EFCoreAnima/AppDbContext.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using EFCoreAnima.Classes;
 using Microsoft.EntityFrameworkCore;
 
 namespace EFCoreAnima
@@ -11,10 +11,19 @@ namespace EFCoreAnima
     public class AppDbContext : DbContext
     {
         public DbSet<Animal> Animals { get; set; }
+        public DbSet<Tierheim> Tierheime { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=animals.db");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Konfiguration der Beziehung (sofern vorhanden)
+            modelBuilder.Entity<Animal>()
+                .HasOne(a => a.Tierheim)
+                .WithMany(t => t.AnimalsList)
+                .HasForeignKey(a => a.TierheimId);
         }
     }
 }
